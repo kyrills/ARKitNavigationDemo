dff00944b5292cb5e7e8dc0aa54225922f22315c
Michiel Everts <michieleverts-18@hotmail.nl>
2017-11-17 22:54:10 +0100

    added extension

---
+++ ARKitNavigationDemo/ARKitNavigationDemo/Extensions/AnimationExtension.swift
@@ -0,0 +1,40 @@
+//
+//  animationExtension.swift
+//  ARKitNavigationDemo
+//
+//  Created by Michiel Everts on 17-11-17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AnimationExtension {
+    
+    
+    func animeteBlurIn() {
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+        
+    }
+    
+    func animateBlurout() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+        }) { (succes:Bool) in
+            self.addItemView.removeFromSuperview()
+        }
+    }
+}

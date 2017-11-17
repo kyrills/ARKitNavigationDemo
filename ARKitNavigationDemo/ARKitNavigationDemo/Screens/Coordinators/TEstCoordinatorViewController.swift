5cad2affaa1416e20657e0e841cf88a2875e87e9
Michiel Everts <michieleverts-18@hotmail.nl>
2017-11-18 00:32:21 +0100

    added subView

--- ARKitNavigationDemo/ARKitNavigationDemo/Screens/Coordinators/TEstCoordinatorViewController.swift
+++ ARKitNavigationDemo/ARKitNavigationDemo/Screens/Coordinators/TEstCoordinatorViewController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 class TEstCoordinatorViewController: UIViewController {
 
+    
+    @IBOutlet weak var flightNumberoutlet: UITextField!
+    @IBOutlet weak var flightDateoutlet: UITextField!
+    @IBOutlet var addItemView : UIView!
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    var progressView: UIProgressView?
+    var progressLabel: UILabel?
+    var effect: UIVisualEffect!
+    var timer: Timer!
+    let rect = CGRect(
+        origin: CGPoint(x: -25, y: -100),
+        size: UIScreen.main.bounds.size
+    )
+    
+    
+    override func viewDidAppear(_ _animated: Bool) {
+        super.viewDidAppear(_animated)
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+
+        addItemView.layer.cornerRadius = 5
+        self.animeteBlurIn()
+   
+    }
+ 
+    
+    @IBAction func didPressFlight(_ sender: Any) {
+        animateBlurout()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +54,29 @@ class TEstCoordinatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func animeteBlurIn() {
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+        
     }
-    */
-
+    
+    func animateBlurout() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+        }) { (succes:Bool) in
+            self.addItemView.removeFromSuperview()
+        }
+    }
 }

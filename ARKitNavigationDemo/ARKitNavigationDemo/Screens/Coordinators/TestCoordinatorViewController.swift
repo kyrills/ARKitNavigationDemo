6f88d768863d27afa7fa65f6d55325ed81536721
Michiel Everts <michieleverts-18@hotmail.nl>
2017-11-18 00:32:21 +0100

    added subView

--- ARKitNavigationDemo/ARKitNavigationDemo/Screens/Coordinators/TestCoordinatorViewController.swift
+++ ARKitNavigationDemo/ARKitNavigationDemo/Screens/Coordinators/TestCoordinatorViewController.swift
@@ -6,19 +6,16 @@
 //  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
 //
 
-
 import UIKit
 
-class TestCoordinatorViewController: UIViewController {
+class TEstCoordinatorViewController: UIViewController {
+
     
     @IBOutlet weak var flightNumberoutlet: UITextField!
     @IBOutlet weak var flightDateoutlet: UITextField!
     @IBOutlet var addItemView : UIView!
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
-    @IBOutlet var timeLabel: UILabel!
-    @IBOutlet var flightNumberLabel: UILabel!
     
-    @IBOutlet var contentContainer: UIStackView!
     var progressView: UIProgressView?
     var progressLabel: UILabel?
     var effect: UIVisualEffect!
@@ -28,44 +25,39 @@ class TestCoordinatorViewController: UIViewController {
         size: UIScreen.main.bounds.size
     )
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setGradient()
-        setupTextField()
-    }
-    
     
     override func viewDidAppear(_ _animated: Bool) {
         super.viewDidAppear(_animated)
         effect = visualEffectView.effect
         visualEffectView.effect = nil
+
         addItemView.layer.cornerRadius = 5
         self.animeteBlurIn()
-        self.visualEffectView.isHidden = false
-        
+   
     }
-    @IBAction func startAR(_ sender: Any) {
-        print("start AR")
-    }
+ 
     
     @IBAction func didPressFlight(_ sender: Any) {
         animateBlurout()
-        self.visualEffectView.isHidden = true
-//        if let flightNumber = flightNumberoutlet {
-//            self.flightNumberLabel.text = flightNumber.text
-//        }
     }
     
-    @IBAction func didPressChangeFlight(_ sender: Any) {
-        animeteBlurIn()
-        flightNumberoutlet.becomeFirstResponder()
-        self.visualEffectView.isHidden = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     func animeteBlurIn() {
         self.view.addSubview(addItemView)
         addItemView.center = self.view.center
+        
         addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
         addItemView.alpha = 0
         
@@ -87,45 +79,4 @@ class TestCoordinatorViewController: UIViewController {
             self.addItemView.removeFromSuperview()
         }
     }
-}
-
-
-// MARK: - View setup
-extension TestCoordinatorViewController {
-    
-    func setGradient() {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [Color.blue3.cgColor, Color.blue2.cgColor, Color.blue1.cgColor]
-        self.view.layer.insertSublayer(gradientLayer, at: 0)
-    }
-}
-
-
-// MARK: - INPUT
-extension TestCoordinatorViewController: UITextFieldDelegate {
-    
-    func setupTextField() {
-        flightNumberoutlet.delegate = self
-        flightDateoutlet.delegate = self
-     }
-    
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        flightNumberoutlet.resignFirstResponder()
-        flightDateoutlet.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let flightNumber = flightNumberoutlet {
-            flightNumberLabel.text = flightNumber.text
-        }
-    }
-}
-
-
-// MARK: - data
-extension TestCoordinatorViewController {
-    
 }

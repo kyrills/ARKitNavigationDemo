ec8b56b93d5faae14071c692b81d71192caae873
Ivo Nederlof <ivo.nederlof@dutchmelon.com>
2017-11-18 13:35:18 +0100

    time working

--- ARKitNavigationDemo/ARKitNavigationDemo/Services/SchipholWhenToBeService.swift
+++ ARKitNavigationDemo/ARKitNavigationDemo/Services/SchipholWhenToBeService.swift
@@ -12,30 +12,33 @@ import Alamofire
 
 class SchipholWhenToBeService {
     
-    typealias getTimeToSchipholCompletion = (Bool, Any?) -> Void
+    typealias getTimeToSchipholCompletion = (Bool, String?) -> Void
     
     public static func checkWhentoBeAtSChiphol(flightNumber: String, onCompletion: @escaping getTimeToSchipholCompletion) {
-        let userDetailsParams: Parameters = ["app_id" : "357a3ef1",
-                                             "app_key":"38b338a9405d784b4cc051bb7d3372d9",
-                                             "Accept" : "application/json",
-                                             "ResourceVersion" : "v1"]
-        let url = "https://api-acc.schiphol.nl/when-to-be-at-schiphol/private/BA2765?checkinRequired=false&debug=false"
+        let userDetailsParams: HTTPHeaders = ["app_id" : "7f76802a",
+                                              "app_key":"07440013e362c9b76cf05a5d1f12dd4b",
+                                              "Accept" : "application/json",
+                                              "ResourceVersion" : "v1"]
+        let url = "https://api-acc.schiphol.nl/when-to-be-at-schiphol/private/"+flightNumber+"?checkinRequired=true&debug=false"
         print(url)
-        Alamofire.request(url,
-              method: .get,
-              parameters: userDetailsParams,
-              encoding: JSONEncoding.default).responseJSON { (response) in
-                switch response.result {
-                case .success(let jsonData):
-                    print(jsonData)
-                    
-                    onCompletion(true, jsonData)
-                    break
-
-                case .failure(let error):
-                    onCompletion(false, nil)
-                    print("error \(error)")
+        
+        Alamofire.request(url,  headers: userDetailsParams).responseJSON { (response) in
+            switch response.result {
+            case .success(let jsonData):
+                print(jsonData)
+                if let dict = jsonData as? NSDictionary,
+                    let recTime = dict["recommendedTimes"] as? NSDictionary {
+                    if let securityTime = recTime["recommendedArrivalAtSecurity"] as? String {
+                        onCompletion(true, securityTime)
+                    }
                 }
+                break
+                
+            case .failure(let error):
+                onCompletion(false, nil)
+                print("error \(error)")
+            }
         }
     }
 }
+
